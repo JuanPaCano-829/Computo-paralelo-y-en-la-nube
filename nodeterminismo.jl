ce587327e4548d42e860f26215858e8d634b838a
juanpa829 <99041881+juanpa829@users.noreply.github.com>
2026-02-03 19:12:50 -0600

    código para no determinismo

---
+++ nodeterminismo.jl
@@ -0,0 +1,30 @@
+using .Threads
+
+function thread_counting(n)
+    a = zeros(Int, 1)
+    """para multiples hilos dentro de un solo proceso"""
+    lk = ReentrantLock()
+    @threads for i=1:n
+        lock(lk) do 
+            a[1] += 1
+        end
+        println("soy hilo ", 
+            Threads.threadid(), 
+            "valor de a[1] = ", 
+            a[1], 
+            "\n")
+    end
+    return a[1]
+end
+
+function simple_counting(n)
+    """Julia no arranca con zeros tenemos que ponerle unos 
+    Este vector es para pasar la referencia al vector 
+    
+    """
+    a = zeros(Int, 1)
+    for i=1:n
+        a[1] += 1
+    end
+    return a[1]
+end
